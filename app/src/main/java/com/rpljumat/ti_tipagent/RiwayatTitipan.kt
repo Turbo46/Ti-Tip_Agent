685f96ce0b675e6d9cf719ee736f03cc93d2853e
fatur <faturrahmanstoffel90@gmail.com>
2020-11-24 13:42:50 +0900

    history agent

---
+++ app/src/main/java/com/rpljumat/ti_tipagent/RiwayatTitipan.kt
@@ -0,0 +1,11 @@
+package com.rpljumat.ti_tipagent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RiwayatTitipan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riwayat_titipan)
+    }
+}
