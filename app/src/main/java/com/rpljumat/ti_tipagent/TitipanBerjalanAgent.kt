5fb9a6c6b8a1ae8f28d1bf564dac7628f1124d75
fatur <faturrahmanstoffel90@gmail.com>
2020-11-24 21:52:42 +0900

    Titipan berjalan

---
+++ app/src/main/java/com/rpljumat/ti_tipagent/TitipanBerjalanAgent.kt
@@ -0,0 +1,11 @@
+package com.rpljumat.ti_tipagent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TitipanBerjalanAgent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_titipan_berjalan_agent)
+    }
+}
