b359e856b5419d09a616e13674d53fe778df12dc
fatur <faturrahmanstoffel90@gmail.com>
2020-12-01 18:12:59 +0900

    Registrasi user tambah tombol login di samping register

--- app/src/main/java/com/rpljumat/ti_tipagent/Registrasi_Agent.kt
+++ app/src/main/java/com/rpljumat/ti_tipagent/Registrasi_Agent.kt
@@ -3,9 +3,12 @@ package com.rpljumat.ti_tipagent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class Registrasi_Agent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrasi__agent)
+
     }
 }
