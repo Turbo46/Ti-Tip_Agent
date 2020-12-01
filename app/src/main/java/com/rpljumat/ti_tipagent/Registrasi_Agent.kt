e6872a36224dae1c88dc0b464ace340654caf04e
fatur <faturrahmanstoffel90@gmail.com>
2020-12-01 17:23:57 +0900

    Registrasi dari agent dan penambahan string

---
+++ app/src/main/java/com/rpljumat/ti_tipagent/Registrasi_Agent.kt
@@ -0,0 +1,11 @@
+package com.rpljumat.ti_tipagent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registrasi_Agent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrasi__agent)
+    }
+}
